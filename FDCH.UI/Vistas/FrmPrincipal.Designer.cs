8dcc84ab041d101fc57563cc0e205507f6545f03
ErikJC25 <erikyumi110@gmail.com>
2025-08-20 17:52:03 -0500

    ...
    ADADADA

--- FDCH.UI/Vistas/FrmPrincipal.Designer.cs
+++ FDCH.UI/Vistas/FrmPrincipal.Designer.cs
@@ -50,7 +50,7 @@
             this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Top;
             this.pictureBox1.Location = new System.Drawing.Point(0, 0);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(322, 129);
+            this.pictureBox1.Size = new System.Drawing.Size(322, 570);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
