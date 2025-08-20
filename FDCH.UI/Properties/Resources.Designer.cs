1210615c75fc5798c328081211afbf1045e13221
ErikJC25 <erikyumi110@gmail.com>
2025-08-20 18:39:32 -0500

    Se eliminaron archivos binarios y temporales del repositorio

--- FDCH.UI/Properties/Resources.Designer.cs
+++ FDCH.UI/Properties/Resources.Designer.cs
@@ -69,5 +69,15 @@ namespace FDCH.UI.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
+        
+        /// <summary>
+        ///   Busca un recurso adaptado de tipo System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap Logo_FDCH1 {
+            get {
+                object obj = ResourceManager.GetObject("Logo_FDCH1", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
