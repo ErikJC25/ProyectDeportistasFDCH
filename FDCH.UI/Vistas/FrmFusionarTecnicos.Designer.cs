d1e8e702194b4e4f6fa249ebcb4a9617e979e21d
panlorex <mate-lorei@hotmail.com>
2025-09-09 02:01:50 -0500

    Gestión de Técnicos Completa

---
+++ FDCH.UI/Vistas/FrmFusionarTecnicos.Designer.cs
@@ -0,0 +1,221 @@
+﻿namespace FDCH.UI.Vistas
+{
+    partial class FrmFusionarTecnicos
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dataGridViewSeleccionados = new System.Windows.Forms.DataGridView();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtNuevoNombre = new System.Windows.Forms.TextBox();
+            this.btnFusionar = new System.Windows.Forms.Button();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.col_Nombre = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.col_Disciplinas = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colIdTecnico = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewSeleccionados)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.SteelBlue;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1196, 65);
+            this.panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial Black", 19.8F, System.Drawing.FontStyle.Bold);
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(443, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(290, 38);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Fusionar Técnicos";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 13.8F, System.Drawing.FontStyle.Bold);
+            this.label2.ForeColor = System.Drawing.Color.SteelBlue;
+            this.label2.Location = new System.Drawing.Point(311, 96);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(365, 22);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Técnicos seleccionados para fusionar";
+            // 
+            // dataGridViewSeleccionados
+            // 
+            this.dataGridViewSeleccionados.AllowUserToAddRows = false;
+            this.dataGridViewSeleccionados.AllowUserToDeleteRows = false;
+            this.dataGridViewSeleccionados.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewSeleccionados.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.col_Nombre,
+            this.col_Disciplinas,
+            this.colIdTecnico});
+            this.dataGridViewSeleccionados.Location = new System.Drawing.Point(315, 135);
+            this.dataGridViewSeleccionados.Name = "dataGridViewSeleccionados";
+            this.dataGridViewSeleccionados.ReadOnly = true;
+            this.dataGridViewSeleccionados.Size = new System.Drawing.Size(591, 210);
+            this.dataGridViewSeleccionados.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial", 13.8F, System.Drawing.FontStyle.Bold);
+            this.label3.ForeColor = System.Drawing.Color.SteelBlue;
+            this.label3.Location = new System.Drawing.Point(311, 367);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(298, 22);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Creación del técnico fusionado";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Arial", 13.8F, System.Drawing.FontStyle.Bold);
+            this.label4.ForeColor = System.Drawing.Color.Black;
+            this.label4.Location = new System.Drawing.Point(311, 422);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(184, 22);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Nombre Completo:";
+            // 
+            // txtNuevoNombre
+            // 
+            this.txtNuevoNombre.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.txtNuevoNombre.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.txtNuevoNombre.Font = new System.Drawing.Font("Arial", 12F);
+            this.txtNuevoNombre.Location = new System.Drawing.Point(315, 459);
+            this.txtNuevoNombre.Name = "txtNuevoNombre";
+            this.txtNuevoNombre.Size = new System.Drawing.Size(591, 26);
+            this.txtNuevoNombre.TabIndex = 5;
+            this.txtNuevoNombre.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtNuevoNombre_KeyPress);
+            // 
+            // btnFusionar
+            // 
+            this.btnFusionar.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.btnFusionar.BackColor = System.Drawing.Color.SteelBlue;
+            this.btnFusionar.FlatAppearance.BorderSize = 0;
+            this.btnFusionar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnFusionar.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnFusionar.ForeColor = System.Drawing.Color.White;
+            this.btnFusionar.Location = new System.Drawing.Point(702, 546);
+            this.btnFusionar.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnFusionar.Name = "btnFusionar";
+            this.btnFusionar.Size = new System.Drawing.Size(204, 40);
+            this.btnFusionar.TabIndex = 28;
+            this.btnFusionar.Text = "Fusionar";
+            this.btnFusionar.UseVisualStyleBackColor = false;
+            this.btnFusionar.Click += new System.EventHandler(this.btnFusionar_Click);
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.btnCancelar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.btnCancelar.FlatAppearance.BorderSize = 0;
+            this.btnCancelar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCancelar.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCancelar.ForeColor = System.Drawing.Color.SteelBlue;
+            this.btnCancelar.Location = new System.Drawing.Point(315, 546);
+            this.btnCancelar.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(204, 40);
+            this.btnCancelar.TabIndex = 29;
+            this.btnCancelar.Text = "Cancelar";
+            this.btnCancelar.UseVisualStyleBackColor = false;
+            this.btnCancelar.Click += new System.EventHandler(this.btnCancelar_Click);
+            // 
+            // col_Nombre
+            // 
+            this.col_Nombre.HeaderText = "NOMBRE COMPLETO";
+            this.col_Nombre.Name = "col_Nombre";
+            this.col_Nombre.ReadOnly = true;
+            this.col_Nombre.Width = 275;
+            // 
+            // col_Disciplinas
+            // 
+            this.col_Disciplinas.HeaderText = "DISCIPLINAS DIRIGIDAS";
+            this.col_Disciplinas.Name = "col_Disciplinas";
+            this.col_Disciplinas.ReadOnly = true;
+            this.col_Disciplinas.Width = 250;
+            // 
+            // colIdTecnico
+            // 
+            this.colIdTecnico.HeaderText = "id_tecnico";
+            this.colIdTecnico.Name = "colIdTecnico";
+            this.colIdTecnico.ReadOnly = true;
+            this.colIdTecnico.Visible = false;
+            // 
+            // FrmFusionarTecnicos
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1196, 706);
+            this.Controls.Add(this.btnCancelar);
+            this.Controls.Add(this.btnFusionar);
+            this.Controls.Add(this.txtNuevoNombre);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.dataGridViewSeleccionados);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "FrmFusionarTecnicos";
+            this.Text = "FrmFusionarTecnicos";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewSeleccionados)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridView dataGridViewSeleccionados;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtNuevoNombre;
+        private System.Windows.Forms.Button btnFusionar;
+        private System.Windows.Forms.Button btnCancelar;
+        private System.Windows.Forms.DataGridViewTextBoxColumn col_Nombre;
+        private System.Windows.Forms.DataGridViewTextBoxColumn col_Disciplinas;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colIdTecnico;
+    }
+}
---
+++ FDCH.UI/Vistas/FrmFusionarTecnicos.cs
@@ -0,0 +1,141 @@
+﻿using FDCH.Entidades;
+using FDCH.Logica;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FDCH.UI.Vistas
+{
+    public partial class FrmFusionarTecnicos : Form
+    {
+        private readonly List<TecnicoRowDto> _seleccionados;
+        private readonly FrmPrincipal _frmPrincipal;
+        private readonly Cls_Puente _puente = new Cls_Puente();
+        public FrmFusionarTecnicos(List<TecnicoRowDto> seleccionados, FrmPrincipal principal)
+        {
+            InitializeComponent();
+            _seleccionados = seleccionados ?? throw new ArgumentNullException(nameof(seleccionados));
+            _frmPrincipal = principal ?? throw new ArgumentNullException(nameof(principal));
+
+            dataGridViewSeleccionados.AutoGenerateColumns = false;
+
+            // Cargar en grid y prefills
+            CargarGridDesdeSeleccionados();
+
+            // Pre-llenar el textbox del nuevo técnico con el primer seleccionado (puedes cambiar)
+            if (_seleccionados.Count > 0)
+            {
+                txtNuevoNombre.Text = _seleccionados[0].NombreCompleto ?? string.Empty;
+            }
+        }
+
+        private void txtNuevoNombre_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            // Verifica si la tecla presionada es la tecla Enter
+            if (e.KeyChar == (char)Keys.Enter)
+            {
+                // Llama al método que maneja el clic del botón
+                btnFusionar_Click(sender, e);
+
+                // Importante: establece e.Handled en true para evitar el sonido
+                // y para que la tecla Enter / Tab no se procese como un carácter de entrada.
+                e.Handled = true;
+            }
+
+            if (e.KeyChar == (char)Keys.Tab)
+            {
+                // Pone el foco de control en el boton Agregar
+                btnFusionar.Focus();
+
+                // Importante: establece e.Handled en true para evitar el sonido
+                // y para que la tecla Enter / Tab no se procese como un carácter de entrada.
+                e.Handled = true;
+            }
+        }
+
+        private void CargarGridDesdeSeleccionados()
+        {
+            dataGridViewSeleccionados.Rows.Clear();
+
+            foreach (var s in _seleccionados)
+            {
+                int idx = dataGridViewSeleccionados.Rows.Add();
+                var row = dataGridViewSeleccionados.Rows[idx];
+
+                // Asegúrate de que el designer tiene columnas con estos Name:
+                // col_Cedula (si no existe para técnicos se puede omitir), col_Nombre, col_Disciplinas, colIdTecnico (oculta)
+                row.Cells["col_Nombre"].Value = s.NombreCompleto ?? string.Empty;
+                row.Cells["col_Disciplinas"].Value = s.DisciplinasDirigidas ?? string.Empty;
+                row.Cells["colIdTecnico"].Value = s.IdTecnico;
+
+                // guardamos el DTO por si lo necesitamos
+                dataGridViewSeleccionados.Rows[idx].Tag = s;
+            }
+
+            dataGridViewSeleccionados.ClearSelection();
+        }
+
+        private void btnFusionar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string nuevoNombre = txtNuevoNombre.Text?.Trim();
+                if (string.IsNullOrWhiteSpace(nuevoNombre))
+                {
+                    MessageBox.Show("Ingrese el nombre completo del técnico resultante.", "Validación", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return;
+                }
+
+                // Confirmación
+                var conf = MessageBox.Show("¿Confirma la fusión de los técnicos seleccionados en un único técnico nuevo?", "Confirmar Fusión", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                if (conf != DialogResult.Yes) return;
+
+                // Preparar lista de ids viejos
+                var idsViejos = _seleccionados.Select(x => x.IdTecnico).Where(id => id > 0).Distinct().ToList();
+                if (idsViejos.Count < 2)
+                {
+                    MessageBox.Show("Se requieren al menos dos técnicos válidos para fusionar.", "Validación", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return;
+                }
+
+                // Construir entidad nuevo técnico
+                Tecnico nuevo = new Tecnico { nombre_completo = nuevoNombre };
+
+                // Obtener id usuario logeado desde FrmPrincipal
+                int idUsuario = 0;
+                if (_frmPrincipal?._usuarioAutenticado != null) idUsuario = _frmPrincipal._usuarioAutenticado.id_usuario;
+
+                // Llamar a la lógica / DB
+                bool ok = _puente.FusionarTecnicosCrearNuevoYReasignar(idsViejos, nuevo, idUsuario);
+
+                if (!ok)
+                {
+                    MessageBox.Show("Ocurrió un error al intentar fusionar los técnicos. Revise logs.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+
+                MessageBox.Show("Fusión completada correctamente.", "Éxito", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                // Volver a gestión para refrescar
+                _frmPrincipal.AbrirFormularioEnPanel(new FrmGestionarTecnicos(_frmPrincipal));
+                this.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error en la fusión: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            _frmPrincipal.AbrirFormularioEnPanel(new FrmGestionarTecnicos(_frmPrincipal));
+            this.Close();
+        }
+    }
+}
